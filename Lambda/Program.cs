36f1d2645b4e52eee10ac77a84ddc5450cc0c8e6
Chablo <scorpiiix@gmail.com>
2018-05-28 14:42:49 +0200

    Q2-Lambda

---
+++ Lambda/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        delegate int del(int i);
+        static void Main(string[] args)
+        {
+            del myDelegate = x => x * x;
+            Console.WriteLine(myDelegate(5));
+        }
+    }
+}
