c87c8d4c0a952024c4571501ad4238fb1924c584
Chablo <scorpiiix@gmail.com>
2018-05-28 14:48:53 +0200

    Q3-AnonymousType

---
+++ AnonymousType/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new { id = 1, name = "BoseQC35" };
+            Console.WriteLine("L'id du produit est : " + product.id + " et le nom du produit est : " + product.name);
+        }
+    }
+}
