67c9a8fb89504d507b6451fcf0f63ee7e3ceaaa4
Chablo <scorpiiix@gmail.com>
2018-05-28 14:36:34 +0200

    Q1-Delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate int myDelegate(int nb1, int nb2);
+
+        static void Main(string[] args)
+        {
+            //myDelegate d = new myDelegate(method);
+            myDelegate d = method;
+            Console.WriteLine(d(5,2));
+        }
+
+        static private int method(int v1, int v2) {
+            return v1 + v2;
+        }
+    }
+}
